1197b604f1ffa0c624ee27c0ea10ea4cf87572b7
Andrea <giovandrea2@gmail.com>
2014-09-03 17:28:50 +0200

    inserite nuove label nel view dell'admin

--- MusicAMM/php/view/amministratore/crea_cd.php
+++ MusicAMM/php/view/amministratore/crea_cd.php
@@ -2,17 +2,23 @@
     <h3>Inserisci nuovo cd</h3>
     <form method="post" action="amministratore/crea_cd">
         <input type="hidden" name="cmd" value="nuovo_cd"/>
-        <label for="caratterizzazione">Caratterizzazione</label>
-        <select name="caratterizzazione" id="caratterizzazione">
+        <label for="artista">Artista</label>
+        <select name="artista" id="artista">
             <?php foreach ($caratterizzazioni as $caratterizzazione) { ?>
                 <option value="<?= $caratterizzazione->getId() ?>" >
-                    <?= $caratterizzazione->getArtista()->getNome() . " " . $caratterizzazione->getTitolo() 
+                    <?= $caratterizzazione->getArtista()->getNome()
                     ?></option>
             <?php } ?>
         </select>
         <br/>
-        <label for="anno">Anno</label>
-        <input type="anno" name="anno" id="anno"/>
+        	<label for="titolo">Titolo</label>
+        	<input type="titolo" name="titolo" id="titolo"/>
+	<br/>
+		<label for="anno">Anno</label>
+        	<input type="anno" name="anno" id="anno"/>
+	<br/>
+		<label for="prezzo">Prezzo</label>
+        	<input type="prezzo" name="prezzo" id="prezzo"/>
         <div class="btn-group">
             <button type="submit" name="cmd" value="cd_nuovo">Inserisci</button>
             <button type="submit" name="cmd" value="a_annulla">Annulla</button>
